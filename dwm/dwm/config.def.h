350706e1eee9fb2b7f0acb5c4741f200f631fe0b
coolport <aidanalcayde@gmail.com>
2025-05-04 23:51:41 +0800

    tmux binds!
    deftmux = new [numbered] sesh
    tmux = dev
    tmux2 = serv

--- dwm/dwm/config.def.h
+++ dwm/dwm/config.def.h
@@ -71,13 +71,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+// static const char *tmux[] = { "st", "-e", "tmux new-session -A -s dev", NULL }; 
+static const char *deftmux[] = { "st", "-e", "tmux" , NULL };
+static const char *tmux[] = { "st", "-e", "tmux", "new-session", "-A", "-s", "dev", NULL };
+static const char *tmux2[] = { "st", "-e", "tmux", "new-session", "-A", "-s", "serv", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {.i = 23} },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_n,       spawn,         SHCMD("firefox") },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = deftmux } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = tmux } },
+	{ MODKEY,                       XK_m,      spawn,          {.v = tmux2 } },
+	{ MODKEY,                       XK_n,      spawn,          SHCMD("firefox") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -85,13 +91,13 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY|ControlMask,           XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
   { MODKEY,                       XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
